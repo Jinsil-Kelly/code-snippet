c272dc5fd593234e5dfbff706380441ab95a3307
jinsil.hwang <jinsil.kelly.hwang@gmail.com>
2019-10-02 12:52:12 +0900

    set sample route and redux for initial structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import WritePage from './pages/WritePage';
 
 const App = () => {
-  return <div>Hello :)</div>;
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postId" />
+    </>
+  );
 };
 
 export default App;
